4ef84667539a0ac5ce86e0edf078aaed8ec05541
Adrian Steffan <adrian.steffan@hotmail.de>
2022-12-20 17:27:39 +0100

    exploratory validation trial analysis

---
+++ validation.R
@@ -0,0 +1,22 @@
+# A small script to report the validation data produced by webgazer for the lmu sample
+library(tidyverse)
+
+validation_data <- read.csv("lmu_validation_data.csv")
+validation_data <- validation_data[,-which(names(validation_data) %in% c("X"))]
+
+mean_offset_x_percent <- mean(abs(validation_data$avg_offset_x_percent))
+sd_offset_x_percent <- sd(abs(validation_data$avg_offset_x_percent))
+mean_offset_y_percent <- mean(abs(validation_data$avg_offset_y_percent))
+sd_offset_y_percent <- sd(abs(validation_data$avg_offset_y_percent))
+
+deterioration <- validation_data |> 
+  pivot_wider(names_from = index, values_from = -c(index, subid)) |>
+  mutate(y_det = abs(avg_offset_y_percent_1) - abs(avg_offset_y_percent_0),
+         x_det = abs(avg_offset_x_percent_1) - abs(avg_offset_x_percent_0)) |> 
+  select(subid, y_det, x_det)
+
+mean_x_det <- mean(deterioration$x_det)
+t.test(deterioration$x_det, mu = 0, alternative = "two.sided")
+
+mean_y_det <- mean(deterioration$y_det)
+t.test(deterioration$y_det, mu = 0, alternative = "two.sided")
