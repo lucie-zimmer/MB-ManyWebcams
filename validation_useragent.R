7713cefa22107163b7f5c2f72287dc6f75e2ada9
Adrian Steffan <adrian.steffan@hotmail.de>
2023-06-19 02:01:17 +0200

    Prepare reporting of device information

--- validation_useragent.R
+++ validation_useragent.R
@@ -1,4 +1,4 @@
-# A small script to report the validation data produced by webgazer
+# A small script to report the validation and device data produced by webgazer
 library(dplyr)
 
 data_all <- read.csv("./extraction_from_raw/data_validation_useragent.csv")
@@ -10,9 +10,13 @@ data <- data_all %>% filter(!(subid %in% excluded_clean))
 
 
 mean_offset_x_percent <- mean(c(abs(data$avg_offset_x_percent_0), abs(data$avg_offset_x_percent_1)))
+mean_offset_x_percent 
 sd_offset_x_percent <- sd(c(abs(data$avg_offset_x_percent_0), abs(data$avg_offset_x_percent_1)))
+sd_offset_x_percent
 mean_offset_y_percent <- mean(c(abs(data$avg_offset_y_percent_0), abs(data$avg_offset_y_percent_1)))
+mean_offset_y_percent
 sd_offset_y_percent <- sd(c(abs(data$avg_offset_y_percent_0), abs(data$avg_offset_y_percent_1)))
+sd_offset_y_percent
 
 deterioration <- validation_data %>% 
   mutate(y_det = abs(avg_offset_y_percent_1) - abs(avg_offset_y_percent_0),
@@ -33,4 +37,13 @@ t.test(deterioration$y_det, mu = 0, alternative = "two.sided")
 data <- data %>% mutate(screen_dimension = paste(window_width, window_height, sep="x"))
 
 table(data$screen_dimension)
+table(data$browser_family)
+
+table(data$os_name_for_study)
+
+data_os_renamed <- data
+data_os_renamed <- data_os_renamed %>% mutate(os_name_for_study = ifelse(startsWith(os_name_for_study, "macOS"), "macOS", os_name_for_study)) # Keep this line in to treat all macOS Versions the same
+table(data_os_renamed$os_name_for_study)
+data_os_renamed <- data_os_renamed %>% mutate(os_name_for_study = ifelse(startsWith(os_name_for_study, "Windows"), "Windows", os_name_for_study)) # Keept his line to treat all Windows Versions the same
+table(data_os_renamed$os_name_for_study)
 
